c669a85fa7c0ffbe5267c0a4dbca85acd85aca41
Ry Ferguson <rytf12@gmail.com>
2023-12-01 19:36:52 +0000

    create home screen and a nav to switch between home and login

--- src/App.js
+++ src/App.js
@@ -1,10 +1,48 @@
 import './App.css';
+import { useState } from 'react';
+import Home from './Home';
 import Login from './Login';
 
 function App() {
+
+  //
+  const [currScreen, setCurrScreen] = useState('home');
+
+  //
+  const changeScreen = (e, screen) => {
+    e.preventDefault();
+    setCurrScreen(screen);
+  }
+
+  //
+  function CurrScreen() {
+    let screen = <Home/>
+
+    if(currScreen == 'login') {
+      screen = <Login/>
+    }
+
+    return (
+      <div>
+        {screen}
+      </div>
+    );
+  }
+
+  //
   return (
     <div>
-      <Login />
+      <div>
+        <ul>
+          <li>
+            <a href="#" onClick={e => changeScreen(e, 'home')}>Home</a>
+          </li>
+          <li>
+            <a href="#" onClick={e => changeScreen(e, 'login')}>Log in</a>
+          </li>
+        </ul>
+      </div>
+      <CurrScreen/>
     </div>
   );
 }
---
+++ src/Home.js
@@ -0,0 +1,11 @@
+export default function Home() {
+
+    //
+    return (
+        <>
+            <div>
+                This is the Chore Wheel home page.
+            </div>
+        </>
+    );
+}
