cfe3491348a2c2416b5dce47a2d1c8c8f2e93889
Ry Ferguson <rytf12@gmail.com>
2023-12-05 00:30:40 +0000

    replace dummy sign up page with sign up form that's connected to database

--- src/SignUp.js
+++ src/SignUp.js
@@ -1,11 +1,80 @@
+import { useState, useRef } from 'react'
+
 export default function SignUp() {
+
+    //
+    const [success, setSuccess] = useState(false)
+    const errorRef = useRef(null)
+    let timeoutId = null
+
+    //
+    const handleSubmit = e => {
+
+        //
+        e.preventDefault();
+
+        //
+        const username = e.target.username.value;
+        const password = e.target.password.value;
+
+        //
+        fetch(
+            "http://127.0.0.1:8000/create-user",
+            {
+                method: "POST",
+                body: JSON.stringify({
+                    username: username,
+                    password: password,
+                }),
+                headers: {
+                    "Content-Type": "application/json",
+                }
+            }
+        )
+        .then((response) => {
+            return response.json()
+        })
+        .then((data) => {
+            if(data) {
+                clearTimeout(timeoutId)
+                errorRef.current.innerHTML = data.detail
+                errorRef.current.style.display = 'block'
+                timeoutId = setTimeout(
+                    () => {
+                        errorRef.current.style.display = 'none'
+                    },
+                    4000,
+                )
+            }
+            else {
+                setSuccess(true)
+            }
+        })
+        .catch((error) => console.log(`error: ${error}`));
+    };
 
     //
     return (
         <>
-            <div>
-                There currently is no sign up form, but you can log in as "johndoe" using the password "secret".
-            </div>
+        {success ? (
+            <div>Sign up successful! You can now log in.</div>
+        ) : (
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label htmlFor="username">Username</label>
+                    <input id="username" name="username"/>
+                </div>
+                <div>
+                    <label htmlFor="password">Password</label>
+                    <input id="password" name="password" type="password"/>
+                </div>
+                <div>
+                    <input type="submit" value="Sign Up"/>
+                </div>
+                <div ref={errorRef} className='hidden-error'>
+                </div>
+            </form>
+        )}
         </>
     );
 }
